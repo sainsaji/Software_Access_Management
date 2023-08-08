98618ac8bb58bd90996f77d9cc00d6a4b6f279eb
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-08-08 16:40:39 +0530

    added side panels and selection

---
+++ UserDashBoard.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace File_Acess_Management
+{
+    partial class UserDashBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserDashBoard));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.requestPanel = new System.Windows.Forms.Panel();
+            this.requestLbl = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.informationPanel = new System.Windows.Forms.Panel();
+            this.userInfoLbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logoPicBox = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.statusPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.leftPanel.SuspendLayout();
+            this.requestPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.informationPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPicBox)).BeginInit();
+            this.statusPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.leftPanel);
+            this.panel1.Controls.Add(this.logoPicBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.leftPanel.Controls.Add(this.statusPanel);
+            this.leftPanel.Controls.Add(this.requestPanel);
+            this.leftPanel.Controls.Add(this.informationPanel);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 50);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(200, 400);
+            this.leftPanel.TabIndex = 1;
+            // 
+            // requestPanel
+            // 
+            this.requestPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.requestPanel.Controls.Add(this.requestLbl);
+            this.requestPanel.Controls.Add(this.pictureBox2);
+            this.requestPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.requestPanel.Location = new System.Drawing.Point(0, 49);
+            this.requestPanel.Name = "requestPanel";
+            this.requestPanel.Size = new System.Drawing.Size(200, 49);
+            this.requestPanel.TabIndex = 1;
+            this.requestPanel.Click += new System.EventHandler(this.requestPanel_Click);
+            this.requestPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.requestPanel_Paint);
+            // 
+            // requestLbl
+            // 
+            this.requestLbl.AutoSize = true;
+            this.requestLbl.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.requestLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.requestLbl.Location = new System.Drawing.Point(42, 10);
+            this.requestLbl.Name = "requestLbl";
+            this.requestLbl.Size = new System.Drawing.Size(132, 30);
+            this.requestLbl.TabIndex = 2;
+            this.requestLbl.Text = "Raise Request";
+            this.requestLbl.Click += new System.EventHandler(this.requestLbl_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // informationPanel
+            // 
+            this.informationPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.informationPanel.Controls.Add(this.userInfoLbl);
+            this.informationPanel.Controls.Add(this.pictureBox1);
+            this.informationPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.informationPanel.Location = new System.Drawing.Point(0, 0);
+            this.informationPanel.Name = "informationPanel";
+            this.informationPanel.Size = new System.Drawing.Size(200, 49);
+            this.informationPanel.TabIndex = 0;
+            this.informationPanel.Click += new System.EventHandler(this.informationPanel_Click);
+            // 
+            // userInfoLbl
+            // 
+            this.userInfoLbl.AutoSize = true;
+            this.userInfoLbl.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInfoLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.userInfoLbl.Location = new System.Drawing.Point(42, 9);
+            this.userInfoLbl.Name = "userInfoLbl";
+            this.userInfoLbl.Size = new System.Drawing.Size(154, 30);
+            this.userInfoLbl.TabIndex = 0;
+            this.userInfoLbl.Text = "User Information";
+            this.userInfoLbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logoPicBox
+            // 
+            this.logoPicBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoPicBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPicBox.Image")));
+            this.logoPicBox.Location = new System.Drawing.Point(0, 0);
+            this.logoPicBox.Name = "logoPicBox";
+            this.logoPicBox.Size = new System.Drawing.Size(200, 50);
+            this.logoPicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoPicBox.TabIndex = 0;
+            this.logoPicBox.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 450);
+            this.panel2.TabIndex = 1;
+            // 
+            // statusPanel
+            // 
+            this.statusPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.statusPanel.Controls.Add(this.label1);
+            this.statusPanel.Controls.Add(this.pictureBox3);
+            this.statusPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusPanel.Location = new System.Drawing.Point(0, 98);
+            this.statusPanel.Name = "statusPanel";
+            this.statusPanel.Size = new System.Drawing.Size(200, 49);
+            this.statusPanel.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(42, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Request Status";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(49, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // UserDashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UserDashBoard";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.leftPanel.ResumeLayout(false);
+            this.requestPanel.ResumeLayout(false);
+            this.requestPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.informationPanel.ResumeLayout(false);
+            this.informationPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPicBox)).EndInit();
+            this.statusPanel.ResumeLayout(false);
+            this.statusPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel informationPanel;
+        private System.Windows.Forms.Label userInfoLbl;
+        private System.Windows.Forms.PictureBox logoPicBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel requestPanel;
+        private System.Windows.Forms.Label requestLbl;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel statusPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ UserDashBoard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Acess_Management
+{
+    public partial class UserDashBoard : Form
+    {
+        private int currentSelected = 0;
+        public UserDashBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.White;
+            informationPanel.BackColor = Color.Aqua;
+            statusPanel.BackColor = Color.White;
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.White;
+            informationPanel.BackColor = Color.Aqua;
+            statusPanel.BackColor = Color.White;
+
+        }
+
+        private void informationPanel_Click(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.White;
+            informationPanel.BackColor = Color.Aqua;
+            statusPanel.BackColor = Color.White;
+        }
+
+        private void requestLbl_Click(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.Aqua;
+            informationPanel.BackColor = Color.White;
+            statusPanel.BackColor = Color.White;
+
+        }
+
+        private void requestPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void requestPanel_Click(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.Aqua;
+            informationPanel.BackColor = Color.White;
+            statusPanel.BackColor = Color.White;
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            requestPanel.BackColor = Color.White;
+            informationPanel.BackColor = Color.White;
+            statusPanel.BackColor = Color.Aqua;
+        }
+    }
+}
