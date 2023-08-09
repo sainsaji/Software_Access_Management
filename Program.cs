354acbe5465577b181172be85be42f678cff40fa
Smith C D <Smith.CD@trivium-esolutions.com>
2023-08-09 14:49:39 +0530

    userAuthentication with roles management

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace File_Acess_Management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new UserLogin());
         }
     }
 }
