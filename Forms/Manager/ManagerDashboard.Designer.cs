18858027b6c99d8ef93b478e035e82177332d987
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-08-22 11:49:01 +0530

    Minor Adjustments

--- Forms/Manager/ManagerDashboard.Designer.cs
+++ Forms/Manager/ManagerDashboard.Designer.cs
@@ -71,9 +71,9 @@
             // 
             this.tabTitleLbl.AutoSize = true;
             this.tabTitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabTitleLbl.Location = new System.Drawing.Point(612, 24);
+            this.tabTitleLbl.Location = new System.Drawing.Point(492, 19);
             this.tabTitleLbl.Name = "tabTitleLbl";
-            this.tabTitleLbl.Size = new System.Drawing.Size(108, 20);
+            this.tabTitleLbl.Size = new System.Drawing.Size(136, 25);
             this.tabTitleLbl.TabIndex = 0;
             this.tabTitleLbl.Text = "Tab Title Here";
             // 
@@ -118,7 +118,7 @@
             this.logOutLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.logOutLbl.Location = new System.Drawing.Point(72, 14);
             this.logOutLbl.Name = "logOutLbl";
-            this.logOutLbl.Size = new System.Drawing.Size(52, 17);
+            this.logOutLbl.Size = new System.Drawing.Size(60, 20);
             this.logOutLbl.TabIndex = 4;
             this.logOutLbl.Text = "Logout";
             this.logOutLbl.Click += new System.EventHandler(this.logOut);
@@ -153,7 +153,7 @@
             this.reqStatusLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.reqStatusLbl.Location = new System.Drawing.Point(48, 12);
             this.reqStatusLbl.Name = "reqStatusLbl";
-            this.reqStatusLbl.Size = new System.Drawing.Size(64, 17);
+            this.reqStatusLbl.Size = new System.Drawing.Size(78, 20);
             this.reqStatusLbl.TabIndex = 2;
             this.reqStatusLbl.Text = "User List";
             this.reqStatusLbl.Click += new System.EventHandler(this.reqStatusLbl_Click);
@@ -186,7 +186,7 @@
             this.requestLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.requestLbl.Location = new System.Drawing.Point(48, 12);
             this.requestLbl.Name = "requestLbl";
-            this.requestLbl.Size = new System.Drawing.Size(121, 17);
+            this.requestLbl.Size = new System.Drawing.Size(143, 20);
             this.requestLbl.TabIndex = 2;
             this.requestLbl.Text = "Incoming Request";
             this.requestLbl.Click += new System.EventHandler(this.requestLbl_Click);
@@ -219,7 +219,7 @@
             this.userInfoLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.userInfoLbl.Location = new System.Drawing.Point(48, 11);
             this.userInfoLbl.Name = "userInfoLbl";
-            this.userInfoLbl.Size = new System.Drawing.Size(138, 17);
+            this.userInfoLbl.Size = new System.Drawing.Size(162, 20);
             this.userInfoLbl.TabIndex = 0;
             this.userInfoLbl.Text = "Manager Information";
             // 
@@ -251,7 +251,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1120, 66);
+            this.panel3.Size = new System.Drawing.Size(1120, 63);
             this.panel3.TabIndex = 0;
             // 
             // panel2
@@ -273,7 +273,7 @@
             this.assignedLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.assignedLbl.Location = new System.Drawing.Point(278, 89);
             this.assignedLbl.Name = "assignedLbl";
-            this.assignedLbl.Size = new System.Drawing.Size(121, 20);
+            this.assignedLbl.Size = new System.Drawing.Size(150, 25);
             this.assignedLbl.TabIndex = 1;
             this.assignedLbl.Text = "Assigned Users";
             this.assignedLbl.Click += new System.EventHandler(this.label1_Click);
@@ -297,10 +297,10 @@
             // userListBx
             // 
             this.userListBx.FormattingEnabled = true;
-            this.userListBx.ItemHeight = 20;
+            this.userListBx.ItemHeight = 25;
             this.userListBx.Location = new System.Drawing.Point(282, 117);
             this.userListBx.Name = "userListBx";
-            this.userListBx.Size = new System.Drawing.Size(325, 464);
+            this.userListBx.Size = new System.Drawing.Size(325, 454);
             this.userListBx.TabIndex = 1;
             // 
             // mySqlDataAdapter1
@@ -312,7 +312,7 @@
             // 
             // ManagerDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1120, 657);
             this.Controls.Add(this.panel1);
