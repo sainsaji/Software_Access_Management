8eda34f9e074d71d9df3ee0d82aa4097af223547
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-08-23 10:44:02 +0530

    SoftwareManagement UC Completed

--- Forms/Admin/SoftwareManagement.Designer.cs
+++ Forms/Admin/SoftwareManagement.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.softwareListLbl = new System.Windows.Forms.Label();
             this.softwareListDataGridView = new System.Windows.Forms.DataGridView();
             this.softwareIdlabel = new System.Windows.Forms.Label();
             this.softwareIdText = new System.Windows.Forms.TextBox();
@@ -38,21 +37,8 @@
             this.deleteButton = new System.Windows.Forms.Button();
             this.updateButton = new System.Windows.Forms.Button();
             this.resetButton = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.softwareListDataGridView)).BeginInit();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // softwareListLbl
-            // 
-            this.softwareListLbl.AutoSize = true;
-            this.softwareListLbl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.softwareListLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.softwareListLbl.Location = new System.Drawing.Point(456, 9);
-            this.softwareListLbl.Name = "softwareListLbl";
-            this.softwareListLbl.Size = new System.Drawing.Size(143, 24);
-            this.softwareListLbl.TabIndex = 0;
-            this.softwareListLbl.Text = "List of Softwares";
             // 
             // softwareListDataGridView
             // 
@@ -61,17 +47,17 @@
             this.softwareListDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.softwareListDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.softwareListDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.softwareListDataGridView.Location = new System.Drawing.Point(571, 112);
+            this.softwareListDataGridView.Location = new System.Drawing.Point(443, 168);
             this.softwareListDataGridView.MultiSelect = false;
             this.softwareListDataGridView.Name = "softwareListDataGridView";
             this.softwareListDataGridView.ReadOnly = true;
-            this.softwareListDataGridView.Size = new System.Drawing.Size(617, 568);
+            this.softwareListDataGridView.Size = new System.Drawing.Size(439, 512);
             this.softwareListDataGridView.TabIndex = 1;
             // 
             // softwareIdlabel
             // 
             this.softwareIdlabel.AutoSize = true;
-            this.softwareIdlabel.Location = new System.Drawing.Point(85, 169);
+            this.softwareIdlabel.Location = new System.Drawing.Point(19, 171);
             this.softwareIdlabel.Name = "softwareIdlabel";
             this.softwareIdlabel.Size = new System.Drawing.Size(91, 20);
             this.softwareIdlabel.TabIndex = 2;
@@ -80,22 +66,22 @@
             // softwareIdText
             // 
             this.softwareIdText.Enabled = false;
-            this.softwareIdText.Location = new System.Drawing.Point(231, 166);
+            this.softwareIdText.Location = new System.Drawing.Point(165, 168);
             this.softwareIdText.Name = "softwareIdText";
-            this.softwareIdText.Size = new System.Drawing.Size(279, 26);
+            this.softwareIdText.Size = new System.Drawing.Size(234, 26);
             this.softwareIdText.TabIndex = 3;
             // 
             // softwareNametext
             // 
-            this.softwareNametext.Location = new System.Drawing.Point(231, 228);
+            this.softwareNametext.Location = new System.Drawing.Point(165, 230);
             this.softwareNametext.Name = "softwareNametext";
-            this.softwareNametext.Size = new System.Drawing.Size(279, 26);
+            this.softwareNametext.Size = new System.Drawing.Size(234, 26);
             this.softwareNametext.TabIndex = 5;
             // 
             // softwareNameLabel
             // 
             this.softwareNameLabel.AutoSize = true;
-            this.softwareNameLabel.Location = new System.Drawing.Point(85, 231);
+            this.softwareNameLabel.Location = new System.Drawing.Point(19, 233);
             this.softwareNameLabel.Name = "softwareNameLabel";
             this.softwareNameLabel.Size = new System.Drawing.Size(119, 20);
             this.softwareNameLabel.TabIndex = 4;
@@ -103,7 +89,7 @@
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(89, 317);
+            this.addButton.Location = new System.Drawing.Point(23, 319);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(99, 35);
             this.addButton.TabIndex = 6;
@@ -113,7 +99,7 @@
             // 
             // deleteButton
             // 
-            this.deleteButton.Location = new System.Drawing.Point(299, 317);
+            this.deleteButton.Location = new System.Drawing.Point(233, 319);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(99, 35);
             this.deleteButton.TabIndex = 7;
@@ -123,7 +109,7 @@
             // 
             // updateButton
             // 
-            this.updateButton.Location = new System.Drawing.Point(194, 317);
+            this.updateButton.Location = new System.Drawing.Point(128, 319);
             this.updateButton.Name = "updateButton";
             this.updateButton.Size = new System.Drawing.Size(99, 35);
             this.updateButton.TabIndex = 8;
@@ -133,7 +119,7 @@
             // 
             // resetButton
             // 
-            this.resetButton.Location = new System.Drawing.Point(404, 317);
+            this.resetButton.Location = new System.Drawing.Point(338, 319);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(99, 35);
             this.resetButton.TabIndex = 9;
@@ -141,22 +127,11 @@
             this.resetButton.UseVisualStyleBackColor = true;
             this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel2.Controls.Add(this.softwareListLbl);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1200, 49);
-            this.panel2.TabIndex = 27;
-            // 
             // SoftwareManagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.deleteButton);
@@ -172,16 +147,12 @@
             this.Text = "Software Management";
             this.Load += new System.EventHandler(this.Software_Load);
             ((System.ComponentModel.ISupportInitialize)(this.softwareListDataGridView)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label softwareListLbl;
         private System.Windows.Forms.DataGridView softwareListDataGridView;
         private System.Windows.Forms.Label softwareIdlabel;
         private System.Windows.Forms.TextBox softwareIdText;
@@ -191,6 +162,5 @@
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Button resetButton;
-        private System.Windows.Forms.Panel panel2;
     }
 }
