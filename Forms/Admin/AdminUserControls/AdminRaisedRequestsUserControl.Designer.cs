10b54c6a9edfffbb32e11dd267831f365f482022
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-08-23 10:48:57 +0530

    RaisedRequestsUC Completeted

--- Forms/Admin/AdminUserControls/AdminRaisedRequestsUserControl.Designer.cs
+++ Forms/Admin/AdminUserControls/AdminRaisedRequestsUserControl.Designer.cs
@@ -34,25 +34,31 @@
             // 
             // adminRequestsDataGridView
             // 
-            this.adminRequestsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.adminRequestsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.adminRequestsDataGridView.AllowUserToAddRows = false;
+            this.adminRequestsDataGridView.AllowUserToDeleteRows = false;
+            this.adminRequestsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.adminRequestsDataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
             this.adminRequestsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.adminRequestsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.adminRequestsDataGridView.Location = new System.Drawing.Point(0, 0);
-            this.adminRequestsDataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.adminRequestsDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.adminRequestsDataGridView.MultiSelect = false;
             this.adminRequestsDataGridView.Name = "adminRequestsDataGridView";
+            this.adminRequestsDataGridView.ReadOnly = true;
             this.adminRequestsDataGridView.RowHeadersWidth = 51;
             this.adminRequestsDataGridView.RowTemplate.Height = 24;
-            this.adminRequestsDataGridView.Size = new System.Drawing.Size(1357, 780);
+            this.adminRequestsDataGridView.Size = new System.Drawing.Size(1018, 634);
             this.adminRequestsDataGridView.TabIndex = 3;
+            this.adminRequestsDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.adminRequestsDataGridView_CellClick);
             // 
             // AdminRaisedRequestsUserControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.adminRequestsDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AdminRaisedRequestsUserControl";
-            this.Size = new System.Drawing.Size(1357, 780);
+            this.Size = new System.Drawing.Size(1018, 634);
             ((System.ComponentModel.ISupportInitialize)(this.adminRequestsDataGridView)).EndInit();
             this.ResumeLayout(false);
 
