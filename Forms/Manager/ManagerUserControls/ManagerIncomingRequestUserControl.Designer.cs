251ca55a011f33c8e2aab294cd8e2e65fef65e82
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-09-04 15:02:09 +0530

    Manager Side Approval Completed

--- Forms/Manager/ManagerUserControls/ManagerIncomingRequestUserControl.Designer.cs
+++ Forms/Manager/ManagerUserControls/ManagerIncomingRequestUserControl.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagerIncomingRequestUserControl));
             this.userRequestGridView = new System.Windows.Forms.DataGridView();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.reviewReqBtn = new System.Windows.Forms.Button();
+            this.denyBtn = new System.Windows.Forms.Button();
+            this.acceptBtn = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.userRequestGridView)).BeginInit();
             this.panel1.SuspendLayout();
@@ -57,68 +57,69 @@
             this.userRequestGridView.RowTemplate.Height = 24;
             this.userRequestGridView.Size = new System.Drawing.Size(672, 442);
             this.userRequestGridView.TabIndex = 3;
-            this.userRequestGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.userRequestGridView_CellClick);
+            this.userRequestGridView.SelectionChanged += new System.EventHandler(this.userRequestGridView_SelectionChanged);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.reviewReqBtn);
+            this.panel1.Controls.Add(this.denyBtn);
+            this.panel1.Controls.Add(this.acceptBtn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(672, 41);
             this.panel1.TabIndex = 4;
+            this.panel1.VisibleChanged += new System.EventHandler(this.panel1_VisibleChanged);
             // 
-            // button1
+            // reviewReqBtn
             // 
-            this.button1.BackColor = System.Drawing.Color.LemonChiffon;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(266, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 41);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Review Required";
-            this.button1.UseVisualStyleBackColor = false;
+            this.reviewReqBtn.BackColor = System.Drawing.Color.LemonChiffon;
+            this.reviewReqBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.reviewReqBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.reviewReqBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.reviewReqBtn.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reviewReqBtn.Image = ((System.Drawing.Image)(resources.GetObject("reviewReqBtn.Image")));
+            this.reviewReqBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reviewReqBtn.Location = new System.Drawing.Point(266, 0);
+            this.reviewReqBtn.Name = "reviewReqBtn";
+            this.reviewReqBtn.Size = new System.Drawing.Size(133, 41);
+            this.reviewReqBtn.TabIndex = 8;
+            this.reviewReqBtn.Text = "Review Required";
+            this.reviewReqBtn.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // denyBtn
             // 
-            this.button2.BackColor = System.Drawing.Color.Salmon;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(133, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 41);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Deny";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.denyBtn.BackColor = System.Drawing.Color.Salmon;
+            this.denyBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.denyBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.denyBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.denyBtn.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.denyBtn.Image = ((System.Drawing.Image)(resources.GetObject("denyBtn.Image")));
+            this.denyBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.denyBtn.Location = new System.Drawing.Point(133, 0);
+            this.denyBtn.Name = "denyBtn";
+            this.denyBtn.Size = new System.Drawing.Size(133, 41);
+            this.denyBtn.TabIndex = 7;
+            this.denyBtn.Text = "Deny";
+            this.denyBtn.UseVisualStyleBackColor = false;
+            this.denyBtn.Click += new System.EventHandler(this.denyBtn_Click);
             // 
-            // button3
+            // acceptBtn
             // 
-            this.button3.BackColor = System.Drawing.Color.LightGreen;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button3.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(0, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 41);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Accept";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.acceptBtn.BackColor = System.Drawing.Color.LightGreen;
+            this.acceptBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.acceptBtn.Dock = System.Windows.Forms.DockStyle.Left;
+            this.acceptBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.acceptBtn.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acceptBtn.Image = ((System.Drawing.Image)(resources.GetObject("acceptBtn.Image")));
+            this.acceptBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.acceptBtn.Location = new System.Drawing.Point(0, 0);
+            this.acceptBtn.Name = "acceptBtn";
+            this.acceptBtn.Size = new System.Drawing.Size(133, 41);
+            this.acceptBtn.TabIndex = 6;
+            this.acceptBtn.Text = "Accept";
+            this.acceptBtn.UseVisualStyleBackColor = false;
+            this.acceptBtn.Click += new System.EventHandler(this.acceptBtn_Click);
             // 
             // panel2
             // 
@@ -151,8 +152,8 @@
         private System.Windows.Forms.DataGridView userRequestGridView;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button reviewReqBtn;
+        private System.Windows.Forms.Button denyBtn;
+        private System.Windows.Forms.Button acceptBtn;
     }
 }
