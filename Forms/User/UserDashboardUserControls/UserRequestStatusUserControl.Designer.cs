d1749506a29d21c28fb86fd822573ee93cd2525e
Sain.Saji <Sain.Saji@trivium-esolutions.com>
2023-09-05 12:42:59 +0530

    layout fixes

--- Forms/User/UserDashboardUserControls/UserRequestStatusUserControl.Designer.cs
+++ Forms/User/UserDashboardUserControls/UserRequestStatusUserControl.Designer.cs
@@ -37,7 +37,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.requestStatusPanel.SuspendLayout();
-            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -45,6 +44,7 @@
             // requestStatusPanel
             // 
             this.requestStatusPanel.BackColor = System.Drawing.Color.White;
+            this.requestStatusPanel.Controls.Add(this.dataGridView1);
             this.requestStatusPanel.Controls.Add(this.panel2);
             this.requestStatusPanel.Controls.Add(this.panel1);
             this.requestStatusPanel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -56,7 +56,6 @@
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Location = new System.Drawing.Point(5, 85);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(838, 229);
@@ -69,13 +68,12 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 49);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(838, 229);
+            this.dataGridView1.Size = new System.Drawing.Size(846, 229);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -151,7 +149,6 @@
             this.Load += new System.EventHandler(this.UserRequestStatusUserControl_Load);
             this.VisibleChanged += new System.EventHandler(this.UserRequestStatusUserControl_VisibleChanged);
             this.requestStatusPanel.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
